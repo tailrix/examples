6795dfb23c64183fd7cb736acbf17dd9236dbe1c
luo zhiyu <luozhiyu@gmail.com>
2025-05-04 23:01:08 +0200

    fix the static to dynamic error

--- playground/app/api/features/route.ts
+++ playground/app/api/features/route.ts
@@ -2,6 +2,8 @@ import { getApiKey } from '@/app/actions/apikey';
 import { fetchFeatures } from '@/lib/utils';
 import { NextRequest, NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(req: NextRequest) {
     try {
         const { searchParams } = new URL(req.url);
